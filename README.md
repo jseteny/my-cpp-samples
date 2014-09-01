645a9de74ae9c92e11e9ecd6201838fcf50f7a5d
jseteny <jseteny@gmail.com>
2014-09-01 17:06:12 +0200

    Initial commit with -toString() support working with operator<< inside basic_ostream -TODO: do the same with auto_ptr

---
+++ README.md
@@ -0,0 +1,4 @@
+my-cpp-samples
+==============
+
+After working with Scala, Java, JavaScript I write again some C++, but now in a different style.
---
+++ src/Main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+struct ToString {
+  virtual string toString() const = 0;
+};
+
+class Person : public ToString {
+public:
+    Person(const string& name) : name(name) {
+    }
+
+    string toString() const {
+        return string("People with name: ") + name;
+    }
+
+    const string& getName() const {
+        return name;
+    }
+
+private:
+    string name;
+};
+
+class Appointment : public ToString {
+public:
+    Appointment(const string& name) : people(name){
+    }
+
+    string toString() const {
+        return string("Appointment with ") + people.getName();
+    }
+
+private:
+    Person people;
+};
+
+Appointment* meet(const string& name){
+  return new Appointment(name);
+}
+
+
+template<typename _CharT, typename _Traits>
+    inline basic_ostream<_CharT, _Traits>&
+    operator<<(basic_ostream<_CharT, _Traits>& __out, const ToString* __s) {
+
+      if (!__s){
+	      __out.setstate(ios_base::badbit);
+
+	    } else {
+
+	      const _CharT* str = __s -> toString().c_str();
+	      __ostream_insert(__out, str, static_cast<streamsize>(_Traits::length(str)));
+	    }
+      return __out;
+}
+
+
+int main() {
+  Appointment* appointment = meet("Joe");
+  cout << appointment << endl;
+  delete appointment;
+
+  auto_ptr<Appointment> meeting( meet("Jack") );
+  cout << meeting.get() << endl;
+  return 0;
+}
